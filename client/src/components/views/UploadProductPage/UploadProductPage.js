46190a524d7ffeed794e7b8c0e80017a97ef3827
lpilas24 <leiva20152@gmail.com>
2020-06-12 10:51:55 -0500

    first commit

---
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,126 @@
+import React, {useState} from 'react'
+import { Typography, Button, Form, message, Input, Icon } from 'antd'
+import FileUpload from '../../utils/FileUpload'
+import Axios from 'axios'
+
+const { Title } = Typography
+const { TextArea } = Input
+
+const Continents = [
+    {key:1, value: "Africa"},
+    {key:2, value: "America"},
+    {key:3, value: "Europe"},
+    {key:4, value: "Oceania"},
+    {key:5, value: "Asia"}
+]
+
+const UploadProductPage = (props) => {
+
+    const [TitleValue, setTitleValue] = useState("")
+    const [DescriptionValue, setDescriptionValue] = useState("")
+    const [PriceValue, setPriceValue] = useState(0)
+    const [ContinentValue, setContinentValue] = useState(1)
+
+    const [Images, setImages] = useState([])
+
+    const onTitleChange = (event) => {
+        setTitleValue(event.currentTarget.value)
+    }
+
+    const onDescriptionChange = (event) => {
+        setDescriptionValue(event.currentTarget.value)
+    }
+
+    const onPriceChange = (event) => {
+        setPriceValue(event.currentTarget.value)
+    }
+
+    const onContinentSelectChange = (event) => {
+        setContinentValue(event.currentTarget.value)
+    }
+
+    const updateImages = (newImages) => {
+        setImages(newImages)
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+
+        if(!TitleValue || !DescriptionValue || !PriceValue ||
+            !ContinentValue || !Images){
+                return alert('Fill all the fields first!')
+            }
+
+        const variables = {
+            writer: props.user.userData._id,
+            title: TitleValue,
+            description: DescriptionValue,
+            price: PriceValue,
+            images: Images,
+            continents: ContinentValue
+        }
+        Axios.post('/api/product/uploadProduct', variables)
+            .then(res => {
+                if(res.data.success) {
+                    alert('Product successfully uploaded')
+                    props.history.push('/')
+                } else {
+                    alert('Failed to upload Product')
+                }
+            })
+    }
+
+    return (
+        <div style={{ maxWidth:'700px', margin:'2rem auto'}}>
+            <div style={{ textAlign:'center', marginBottom:'2rem' }}>
+                <Title level={2}> Upload Travel Product </Title>
+            </div>
+
+            <Form onSubmit={onSubmit}>
+
+                {/* Dropzone */}
+                <FileUpload refreshFunction={updateImages}/>
+
+                <br/>
+                <br/>
+                <label>Title</label>
+                <Input
+                    onChange={onTitleChange}
+                    value={TitleValue}
+                />
+                <br/>
+                <br/>
+                <label>Description</label>
+                <TextArea
+                    onChange={onDescriptionChange}
+                    value={DescriptionValue}
+                />
+                <br/>
+                <br/>
+                <label>Price($)</label>
+                <Input
+                    onChange={onPriceChange}
+                    value={PriceValue}
+                />
+                <select onChange={onContinentSelectChange}>
+                    {Continents.map(item => (
+                        <option key={item.key} value={item.key}>
+                            {item.value}
+                        </option>
+                    ))}
+                </select>
+                <br/>
+                <br/>
+
+                <Button
+                    onClick={onSubmit}
+                >
+                    Submit
+                </Button>
+
+            </Form>
+        </div>
+    )
+}
+
+export default UploadProductPage
